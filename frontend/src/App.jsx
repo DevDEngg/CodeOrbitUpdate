3ae7eb951c1c57d5f91d88d3f4e1b71b27709087
DhryXpert <dhairyakhatri83@gmail.com>
2026-06-20 08:42:13 +0530

    Optimized Lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import React, { lazy, Suspense } from 'react'
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import ThemeToggle from './components/ThemeToggle'
+import Login from './pages/Login'
 import './styles/index.css'
 
-const Login = lazy(() => import('./pages/Login'))
 const SignUp = lazy(() => import('./pages/SignUp'))
 const Dashboard = lazy(() => import('./pages/Dashboard'))
 
